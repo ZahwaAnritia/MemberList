f5d5742801ad49631df17845181b85a245115273
ZahwaAnritia <anritia15@gmail.com>
2024-05-20 13:22:06 +0700

    menambahkan akses publik untuk kondisi anak

--- Aggregation/Anak.h
+++ Aggregation/Anak.h
@@ -5,7 +5,14 @@
 using namespace std;
 
 class anak {
-
+public:
+	string nama;
+	anak(string pNama) :nama(pNama) {
+		cout << " Anak \" " << nama << "\"ada\n";
+	}
+	~anak() {
+		cout << " Anak \" " << nama << "\" tidak ada\n";
+	}
 
 };
 
