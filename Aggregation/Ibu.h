a77bbe3a3edcb6e19a6c201722e648abf5829dff
ZahwaAnritia <anritia15@gmail.com>
2024-05-20 13:26:17 +0700

    menambahkan fungsi void

--- Aggregation/Ibu.h
+++ Aggregation/Ibu.h
@@ -21,4 +21,15 @@ public:
 	void cetakAnak();
 };
 
+void ibu::tambahAnak(anak* pAnak) {
+	daftar_anak.push_back(pAnak);
+}
+
+void ibu::cetakAnak() {
+	cout << "Daftar Anak dari ibu \" " << this->nama << "\:\n";
+	for (auto& a : daftar_anak) {
+		cout << a->nama << "\n";
+	}
+	cout << endl;
+
 #endif
