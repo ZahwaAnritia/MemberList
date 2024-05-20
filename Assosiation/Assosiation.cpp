5bd6be6420f9439a3e6bca32d43472edbae92926
ZahwaAnritia <anritia15@gmail.com>
2024-05-20 18:01:23 +0700

    menambahkan fungsi  main

--- Assosiation/Assosiation.cpp
+++ Assosiation/Assosiation.cpp
@@ -57,3 +57,27 @@ void dokter::cetakPasien() {
 	}
 	cout << endl;
 }
+
+int main()
+{
+	dokter* varDokter1 = new dokter("dr.Budi");
+	dokter* varDokter2 = new dokter("dr.Tono");
+	pasien* varPasien1 = new pasien("Budi");
+	pasien* varPasien2 = new pasien("Andi");
+
+	varDokter1->tambahPasien(varPasien1);
+	varDokter1->tambahPasien(varPasien2);
+	varDokter2->tambahPasien(varPasien1);
+
+	varDokter1->cetakPasien();
+	varDokter2->cetakPasien();
+	varPasien1->cetakDokter();
+	varPasien2->cetakDokter();
+
+	delete varDokter1;
+	delete varDokter2;
+	delete varPasien1;
+	delete varPasien2;
+
+	return 0;
+}
